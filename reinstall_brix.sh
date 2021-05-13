e55ecc41de38abaf31d136ffbb7c453d098c5918
GBabio <guadalupe.babio@gmail.com>
2021-05-13 11:57:53 -0400

    create bash reinstall Brix

---
+++ reinstall_brix.sh
@@ -0,0 +1,9 @@
+# type on terminal: source ~/myEnvs/tera/bin/activate
+# to upgarde brix: pip install --upgrade git+https://github.com/CityScope/CS_Brix.git
+pip uninstall cs-brix
+# To install a commit: pip install git+git://github.com/CityScope/CS_Brix.git@319b135b71aba18708e30b776e5b132ad959cb1c
+#
+# To install a branch: 
+pip install -e git://github.com/CityScope/CS_Brix.git@grid_maker#egg=cs_brix
+#
+# This script integrates the indicators and any modification of the grid
